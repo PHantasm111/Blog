040a93134ed0c4029f499836681ddf435fceb8a6
PH <panghanfr@126.com>
2024-07-06 21:52:04 +0200

    create router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,73 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Single from "./pages/Single"
+import Write from "./pages/Write"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/single",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  }
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
 }
 
 export default App;
